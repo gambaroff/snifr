61965c7b99a6d09e7cf6322fcd237e717476644f
Stephania Gambaroff Thomas <stephania@thoughtbot.com>
2017-05-05 15:49:44 -0700

    Use geolocation to show current location.

--- app/screens/counter.js
+++ app/screens/counter.js
@@ -23,13 +23,37 @@ class Counter extends Component {
   static navigationOptions = {
     title: 'Map',
   };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
 
   render() {
-    const {value, increment, decrement} = this.props;
 
     return (
-      <View style={styles.container}>
-        <Text>Map!</Text>
+      <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
+        {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
         <MapView
           style={styles.map}
           initialRegion={{
